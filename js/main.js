fb16460672b94526a2e1bd638550e7616ae03c9a
nursahtuncel <nursahtuncell@gmail.com>
2025-10-24 00:53:45 +0300

    fix: create route

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+// tüm sayfalar için gerekli kodlar buraya gelecek
+
+    
+if(window.location.pathname.includes("home")) {
+// home sayfasında render olacak kodlar buraya gelecek
+ 
+} else if(window.location.pathname.includes("login")) {
+// login sayfasında render olacak kodlar buraya gelecek
+ 
+}
+else if(window.location.pathname.includes("pricing")) {
+
+// pricing sayfasında render olacak kodlar buraya gelecek
+ 
+}
+else if(window.location.pathname.includes("blog")) {
+// blog sayfasında render olacak kodlar buraya gelecek
+ 
+}
+else if(window.location.pathname.includes("services")) {
+// services sayfasında render olacak kodlar buraya gelecek
+ 
+}
+
+
+
+    
+}); 
