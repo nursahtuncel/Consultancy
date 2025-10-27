d14f583fd8102f736f0919c940f0c299113da903
Coders Club <cod@gmail.com>
2025-10-28 02:35:23 +0300

    [can-oc-4-5] Implement the Navbar and Making the Navbar Responsive

--- js/main.js
+++ js/main.js
@@ -1,31 +1,20 @@
-
-
 document.addEventListener("DOMContentLoaded", async () => {
-// tüm sayfalar için gerekli kodlar buraya gelecek
+  // tüm sayfalar için gerekli kodlar buraya gelecek
 
-    
-if(window.location.pathname.includes("home")) {
-// home sayfasında render olacak kodlar buraya gelecek
- 
-} else if(window.location.pathname.includes("login")) {
-// login sayfasında render olacak kodlar buraya gelecek
- 
-}
-else if(window.location.pathname.includes("pricing")) {
-
-// pricing sayfasında render olacak kodlar buraya gelecek
- 
-}
-else if(window.location.pathname.includes("blog")) {
-// blog sayfasında render olacak kodlar buraya gelecek
- 
-}
-else if(window.location.pathname.includes("services")) {
-// services sayfasında render olacak kodlar buraya gelecek
- 
-}
-
-
-
-    
-}); 
+  if (window.location.pathname.includes("home")) {
+    // home sayfasında render olacak kodlar buraya gelecek
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    hamburger.addEventListener("click", () => {
+      navMenu.classList.toggle("active");
+    });
+  } else if (window.location.pathname.includes("login")) {
+    // login sayfasında render olacak kodlar buraya gelecek
+  } else if (window.location.pathname.includes("pricing")) {
+    // pricing sayfasında render olacak kodlar buraya gelecek
+  } else if (window.location.pathname.includes("blog")) {
+    // blog sayfasında render olacak kodlar buraya gelecek
+  } else if (window.location.pathname.includes("services")) {
+    // services sayfasında render olacak kodlar buraya gelecek
+  }
+});
