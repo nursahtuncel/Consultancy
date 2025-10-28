3d71abd774d837aabafdba0e66a59b646929f44b
Coders Club <cod@gmail.com>
2025-10-28 04:28:24 +0300

    [oc-12-13] Implement the Why Us Section and Make the Why Us Section Responsive

--- js/main.js
+++ js/main.js
@@ -1,31 +1,17 @@
-
-
+import { createWhyUsSection } from "./render.js";
 document.addEventListener("DOMContentLoaded", async () => {
-// tüm sayfalar için gerekli kodlar buraya gelecek
+  // tüm sayfalar için gerekli kodlar buraya gelecek
 
-    
-if(window.location.pathname.includes("home")) {
-// home sayfasında render olacak kodlar buraya gelecek
- 
-} else if(window.location.pathname.includes("login")) {
-// login sayfasında render olacak kodlar buraya gelecek
- 
-}
-else if(window.location.pathname.includes("pricing")) {
-
-// pricing sayfasında render olacak kodlar buraya gelecek
- 
-}
-else if(window.location.pathname.includes("blog")) {
-// blog sayfasında render olacak kodlar buraya gelecek
- 
-}
-else if(window.location.pathname.includes("services")) {
-// services sayfasında render olacak kodlar buraya gelecek
- 
-}
-
-
-
-    
-}); 
+  if (window.location.pathname.includes("home")) {
+    // home sayfasında render olacak kodlar buraya gelecek
+    createWhyUsSection();
+  } else if (window.location.pathname.includes("login")) {
+    // login sayfasında render olacak kodlar buraya gelecek
+  } else if (window.location.pathname.includes("pricing")) {
+    // pricing sayfasında render olacak kodlar buraya gelecek
+  } else if (window.location.pathname.includes("blog")) {
+    // blog sayfasında render olacak kodlar buraya gelecek
+  } else if (window.location.pathname.includes("services")) {
+    // services sayfasında render olacak kodlar buraya gelecek
+  }
+});
---
+++ js/render.js
@@ -0,0 +1,43 @@
+const createWhyUsSection = () => {
+  async function renderWhyUs() {
+    try {
+      const response = await fetch("/db.json");
+      const data = await response.json();
+
+      const whyUsSection = document.getElementById("why-us-section");
+
+      const content = data["why-us-textContent"];
+      const stats = data["why-us-stats"];
+
+      whyUsSection.innerHTML = `
+        <div class="why-us-header">
+          <h4 class="why-us-top-header">${content.eyebrow}</h4>
+          <h2 class="why-us-bottom-header">${content.heading}</h2>
+          <p class="why-us-text">
+            ${content.description}
+          </p>
+        </div>
+        <div class="why-us-content"></div>
+      `;
+
+      const whyUsStats = document.querySelector(".why-us-content");
+
+      whyUsStats.innerHTML = stats
+        .map((item) => {
+          return `
+      <div class="stat-item">
+        <span class="stat-value">${item.value}</span>
+        <p class="stat-text">${item.text}</p>
+      </div>
+    `;
+        })
+        .join("");
+    } catch (err) {
+      console.error("JSON verisi yüklenemedi:", err);
+    }
+  }
+
+  renderWhyUs();
+};
+
+export { createWhyUsSection };
