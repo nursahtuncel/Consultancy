72c4ec77ba65be0921fa5a45a102cb31da5b0f30
Adınız <narinm35@gmail.com>
2025-10-30 11:33:18 +0300

    Style ayarları yapıldı ve Butona click özelliği verildi

--- js/main.js
+++ js/main.js
@@ -1,31 +1,25 @@
-import fetchData from './fetch.js';
+import fetchData from "./fetch.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-// tüm sayfalar için gerekli kodlar buraya gelecek
+  // tüm sayfalar için gerekli kodlar buraya gelecek
+  // get-started adında ki buton tıklanınca alert
+  const button = document.querySelector(".m-right button");
 
-    
-if(window.location.pathname.includes("home")) {
-// home sayfasında render olacak kodlar buraya gelecek
- 
-} else if(window.location.pathname.includes("login")) {
-// login sayfasında render olacak kodlar buraya gelecek
- 
-}
-else if(window.location.pathname.includes("pricing")) {
+  if (button) {
+    button.addEventListener("click", () => {
+      alert("Buton Tıklandı");
+    });
+  }
 
-// pricing sayfasında render olacak kodlar buraya gelecek
- 
-}
-else if(window.location.pathname.includes("blog")) {
-// blog sayfasında render olacak kodlar buraya gelecek
- 
-}
-else if(window.location.pathname.includes("services")) {
-// services sayfasında render olacak kodlar buraya gelecek
- 
-}
-
-
-
-    
-}); 
+  if (window.location.pathname.includes("home")) {
+    // home sayfasında render olacak kodlar buraya gelecek
+  } else if (window.location.pathname.includes("login")) {
+    // login sayfasında render olacak kodlar buraya gelecek
+  } else if (window.location.pathname.includes("pricing")) {
+    // pricing sayfasında render olacak kodlar buraya gelecek
+  } else if (window.location.pathname.includes("blog")) {
+    // blog sayfasında render olacak kodlar buraya gelecek
+  } else if (window.location.pathname.includes("services")) {
+    // services sayfasında render olacak kodlar buraya gelecek
+  }
+});
